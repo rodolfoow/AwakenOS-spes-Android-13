cff2da8d9cf9a50f1f28418d5f8762eef0bc571c
rodolfoow <rodolfovdon@gmail.com>
2026-02-09 02:12:37 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# Repo sync
-repo init -u https://github.com/ProjectAwaken/android_manifest -b 13 --git-lfs
+
+# 1. Manifestlarni to'g'irlash
+rm -rf .repo/local_manifests
+mkdir -p .repo/local_manifests
+cp spes.xml .repo/local_manifests/spes.xml
+
+# 2. Repo Init va Sync
+repo init -u https://github.com/ProjectAwaken/android_manifest -b 13 --git-lfs --depth=1
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
-# Spes uchun qurish
+# 3. Qurish (Build)
 . build/envsetup.sh
 lunch awaken_spes-userdebug
 mka bacon
